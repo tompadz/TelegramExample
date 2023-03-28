26fd40d7579fd392b320a7070a6cb319787eb09f
Igor' Zakharov <tmpadz@gmail.com>
2023-03-28 22:50:42 +0300

    Updated libraries, gradle and settings, removed unnecessary files.

--- app/src/main/java/com/ibashkimi/telegram/MainActivity.kt
+++ app/src/main/java/com/ibashkimi/telegram/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.ibashkimi.telegram
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.ibashkimi.telegram.ui.TelegramApp
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @OptIn(ExperimentalCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TelegramApp(this)
-        }
     }
 }
